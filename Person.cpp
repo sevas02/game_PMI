03c82d1a3e57e3cec592ee438a6295f88fdce29f
LubovKos <vsevolod.sum02@gmail.com>
2022-05-10 22:01:46 +0300

    Залиты 'Person.cpp/h'

---
+++ Person.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "Person.h"
+using namespace std;
+
+//функция увелечения маны после удара
+void Person::rise_mana(Person enemy) {
+	if (enemy._hp <= 0) {
+		_mana += 0.5 * _dmg;
+	}
+	else {
+		_mana += 0.3 * _dmg;
+	}
+}
+
+//функция удара
+void Person::deal_dmg(Person& enemy, int dmg) {
+	if (enemy._armor == 1)
+		_armor = 0;
+	else
+		enemy._hp -= dmg;
+	if (enemy._hp <= 0)
+		cout << "умер" << "\n";
+	rise_mana(enemy);
+}
+
+//функция нанесения урона ядом
+void Person::rec_poison_dmg() {
+	if (_time_poison != 0) {
+		_hp -= 5;
+		_time_poison--;
+	}
+}
+
+//функция нанесения урона кровотечением
+void Person::rec_bleed_dmg() {
+	if (_time_bleed != 0) {
+		_hp -= 5;
+		_time_bleed--;
+	}
+}
--- Person.h
+++ Person.h
@@ -1,1 +1,58 @@
-#pragma once
+#ifndef PERSON
+#define PERSON
+
+class Person {
+protected:
+	int _hp; //здоровье
+	int _dmg; //урон
+	double _mana; //мана
+	bool _armor; //щит
+	int _time_bleed; //время кровточения
+	int _time_poison; //время отравления
+public:
+	//Геттеры
+	int hp() { return _hp; }
+	int dmg() { return _dmg; }
+	double mana() { return _mana; }
+	bool armor() { return _armor; }
+	int time_bleed() { return _time_bleed; }
+	int time_poison() { return _time_poison; }
+
+	//Сеттеры
+	void Set_hp(int hp) { _hp = hp; }
+	void Set_dmg(int dmg) { _dmg = dmg; }
+	void Set_mana(double mana) { _mana = mana; }
+	void Set_armor(bool armor) { _armor = armor; }
+	void Set_time_bleed(int time_bleed) { _time_bleed = time_bleed; }
+	void Set_time_poison(int time_poison) { _time_poison = time_poison; }
+
+	//Функции для добавления ходов к значениям отравления и кровотечения
+	void app_time_bleed(int app_time_bleed) { _time_bleed += app_time_bleed; }
+	void app_time_poison(int app_time_poison) { _time_poison += app_time_poison; }
+
+	//пополнение маны
+	void rise_mana(Person enemy);
+
+	//нанесение урона
+	void punch(Person& enemy, int dmg);
+
+	//получение урона (recieve)
+	void rec_dmg(Person enemy) { _hp -= ((_armor == true) ? _armor = false : _dmg); }
+
+	//получение хп (recieve)
+	void rec_hp(int val) { _hp += val; }
+
+	//обнуление маны
+	void null_mana() { _mana = 0; }
+
+	//получение брони
+	void recieve_armor() { _armor = true; }
+
+	//получение урона от отравления
+	void rec_poison_dmg();
+
+	//получение урона от кровотечения
+	void rec_bleed_dmg();
+};
+
+#endif
