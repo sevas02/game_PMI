d2e11a54f4a22749584f7ea7d4061cf708d25e9e
LubovKos <vsevolod.sum02@gmail.com>
2022-06-15 14:00:02 +0300

    стандартный стек

--- Stack.h
+++ Stack.h
@@ -7,29 +7,18 @@
 using namespace std;
 
 template <class T>
-struct note {
-	T val;
-};
-
-template <class T>
-class Stack_arr {
+class Stack {
 private:
-	note<T>* arr;
+	T* arr;
 	const unsigned int size;
 	int top; //индекс верхнего элемента
 public:
 	//конструктор стека
-	Stack_arr(const unsigned int maxSize) :
-		size(maxSize), top(0), arr(new note<T>[maxSize]) {}
+	Stack(const unsigned int maxSize) :
+		size(maxSize), top(0), arr(new T[maxSize]) {}
 
 	//деструктор стека
-	~Stack_arr() { delete[] arr; }
-
-	//перегрузка оператора вывода
-	ostream& operator << (ostream& os, const arr& stk) {
-		os << arr.val;
-		return os;
-	}
+	~Stack() { delete[] arr; }
 
 	//функция добавления элемента
 	void push(const T);
@@ -48,62 +37,38 @@ public:
 };
 
 template <class T>
-void Stack_arr<T>::push(const T newElement) {
-	try {
-		if (top >= size)
-			throw 1;
-		if (top == 0) {
-			arr[top].val = newElement;
-			top++;
-		}
-		else if (arr[top - 1].curMax > newElement) {
-			arr[top].val = newElement;
-			top++;
-		}
-		else {
-			arr[top].val = newElement;
-			top++;
-		}
-	}
-	catch (int i) {
-		cout << "Ошибка №" << i << " переполнение стека\n";
-	}
+void Stack<T>::push(const T newElement) {
+	if (top >= size)
+		throw 1;
+	arr[top] = newElement;
+	top++;
 }
 
 template <class T>
-T Stack_arr<T>::pop() {
-	try {
-		if (top <= 0)
-			throw 2;
-		T elem = arr[top].val;
-		top--;
-		return elem;
-	}
-	catch (int i) {
-		cout << "Ошибка №" << i << " в стеке больше нет элементов\n";
-	}
+T Stack<T>::pop() {
+	if (top <= 0)
+		throw 2;
+	T elem = arr[top];
+	top--;
+	return elem;
+
 }
 
 template <class T>
-T Stack_arr<T>::GetTop() const {
-	try {
-		if (top <= 0)
-			throw 3;
-		return arr[top - 1].val;
-	}
-	catch (int i) {
-		cout << "Ошибка №" << i << " стек пуст!\n";
-	}
+T Stack<T>::GetTop() const {
+	if (top <= 0)
+		throw 3;
+	return arr[top - 1];
 }
 
 
 template <class T>
-unsigned int  Stack_arr<T>::sizeStack() const {
+unsigned int  Stack<T>::sizeStack() const {
 	return top;
 }
 
 template <class T>
-void Stack_arr<T>::printStack() {
+void Stack<T>::printStack() {
 	for (int idx = top - 1; idx >= 0; idx--)
 		cout << "|" << setw(4) << arr[idx] << endl;
 }
